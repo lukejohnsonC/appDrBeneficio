1dd73744618a2673c7199b82c057ebfe510cdf62
Luke <lukejohnson73@hotmail.com>
2019-11-01 16:29:45 -0300

    Implatação do botão Download
    na página Cartão Virtual

--- resources/views/CartaoFarmacia/verCartaoFarmacia.blade.php
+++ resources/views/CartaoFarmacia/verCartaoFarmacia.blade.php
@@ -13,6 +13,7 @@
           </div>
         </div>
         <div class="container">
+          <a href="../public/novo/cartaoFarmacia/cartaofarmacia.png" download='cartaoFarmacia.png' class="botao-laranja">clique aqui para fazer o download</a>
           <p>Apresente esse cartão no balcão ou caixa da farmácia e solicite seu desconto. Em caso de dúvidas, mostre as orientações abaixo ao atendente.</p>
 
           <p><b>Sr. Farmacêutico</b>, o portador desse cartão é beneficiário do programa UNIVERS e seu código de identificação está informado no cartão acima (CPF do portador). Em caso de dúvidas, favor contatar a Central de Atendimento UNIVERS.</p>
