3edeba3f34bfe3b29cd5c937ebcc94a8629ac519
Luke <lukejohnson73@hotmail.com>
2020-01-07 14:05:26 -0300

    Correção número wps / Formulário Orientação nutricional
    corrigi o numero para o wps que estava errado numa page e arrumei o formulário de orientação nutricional

--- resources/views/DiskSaude/orientacaoNutricional.blade.php
+++ resources/views/DiskSaude/orientacaoNutricional.blade.php
@@ -1,48 +1,59 @@
 @extends('estrutura.master') 
 
 @section('conteudo')
+
 <section id="ori_nutricional">
         <div class="container">
         	<h1>Orientação Nutricional</h1>
-            <p>Para agendar uma Orientação Nutricional, por favor preencha o formulário a baixo com o que se pede (todos os campos são obrigatórios)</p>
+            <p>Para agendar uma Orientação Nutricional, por favor preencha o formulário a baixo com o que se pede (todos os campos são obrigatórios) e aguarde nosso retorno.</p>
     
-    		<form action="" method='POST'>
-    			<!-- INCLUINDO NOME E CPF IDADE -->
-    			<label class="col1">
-    				<span>Celular</span>
-    				<input required type="text" maxlength="14" class="form-control cpf-mask" name='celular' placeholder="(00)00000.0000" autocomplete="off">
+    		<form action="http://email.drbeneficio.com.br/orientacaoNutricional.php" method='POST'>
+                <label class="col2">
+                    <span>Nome</span>
+                    <input required type="text" placeholder="Nome completo" name='nome'>
+                </label>
+                <label class="col2">
+                    <span>CPF</span>
+                    <input required type="text" maxlength="11" class="form-control cpf-mask" name='cpf' placeholder="000.000.000-00" autocomplete="off">
+                </label>
+    			<label class="col2">
+    				<span>Celular (com DDD)</span>
+    				<input required type="text" maxlength="11" class="form-control" name='celular' placeholder="(00) 0000-0000" autocomplete="off">
     			</label>
-    			<label class="col1">
+    			<label class="col2">
     				<span>Profissão</span>
-    				<input required type="text" placeholder="Área de atuação">
+    				<input required type="text" placeholder="Área de atuação" name='profissao'>
     			</label>
-    			<label class="col1">
+    			<label class="col2">
     				<span>Altura</span>
-    				<input required type="text" placeholder="Qual a sua altura?">
+    				<input required type="text" placeholder="Qual a sua altura?" name='altura'>
     			</label>
-    			<label class="col1">
+    			<label class="col2">
     				<span>Peso</span>
-    				<input required type="text" placeholder="Quanto você pesa?">
+    				<input required type="text" placeholder="Quanto você pesa?" name='peso'>
     			</label>
     			<label class="col2">
     				<span>Atividade física</span>
-    				<input type="radio" name='aFisica' checked><span>sim</span>
-    				<input type="radio" name='aFisica'><span>não</span>
+    				<input type="radio" name='aFisica' value='sim'><span>sim</span>
+    				<input type="radio" name='aFisica' value='nao'><span>não</span>
     			</label>
     			<label class="col2">
     				<span>Fumante</span>
-    				<input type="radio" name='fumante' checked><span>sim</span>
-    				<input type="radio" name='fumante'><span>não</span>
+    				<input type="radio" name='fumante' value='sim'><span>sim</span>
+    				<input type="radio" name='fumante' value='nao'><span>não</span>
     			</label>
     			<label class="col1">
     				<!-- DENTRO DO HÓRARIO COMERCIAL -->
-    				<span>Melhor hórario para contato telefônico</span>
+    				<span>Melhor hórario para contato telefônico (horário comercial)</span>
     				<input type="time" name="data">
     			</label>
     			<label class="col1">
     				<span>Demanda</span>
-    				<textarea rows='5' required placeholder="Quais são as suas necessidades e para o que você deseja o atendimento ao nutricionista?"></textarea>
+    				<textarea rows='5' name='motivo' required placeholder="Quais são as suas necessidades e para o que você deseja o atendimento ao nutricionista?"></textarea>
     			</label>
+                <label class="col1">
+                    <input type="submit" style='cursor:pointer;'>
+                </label>
     		</form>
               
               <a href="javascript:history.back()" class='pattern'><span><i class="fas fa-undo-alt"></i> voltar ao menu principal</span></a>
