7b9abef4e174213e0af061c5f4d609c163b8167a
Luke <lukejohnson73@hotmail.com>
2020-01-27 18:17:39 -0300

    Jornada do Voucher com Imagem
    pós liberação, imagem do voucher disponível

--- resources/views/CheckupAnual/checkupVale.blade.php
+++ resources/views/CheckupAnual/checkupVale.blade.php
@@ -2,16 +2,43 @@
 
 @section('conteudo')
 
-
-@if ( Session::get('message') != '' )
-      <div id="erro" style="background-color:green;color:white">{{ Session::get('message') }}</div>
-      @endif
+                @if($vale)
+                    <div id="erro" style="background-color:green;color:white">Seu Voucher foi liberado com sucesso!</div>
+                @endif
 
 <section id="vale-checkup">
         <div class="container">
-            <div class=''>
+            <div class='listVoucher'>
                 @if($vale)
-                <h2 style='color:#25d366;'>Voucher válidado com sucesso!</h2>
+                    
+                     <p>Clique no botão a seguir e apresente seu voucher no ato do atendimento na Celulla Mater
+
+                    <button onclick='abrirVoucher()' class='pattern'>Voucher</button>
+                    </p>
+
+                    <script src="//cdn.jsdelivr.net/npm/sweetalert2@8"></script>
+                    <script>
+                    
+                    function abrirVoucher() {
+                        // Para alertas
+                          Swal.fire({
+                          title: '<strong><u>Dr. Benefício - Voucher</u></strong>',
+                          type: 'info',
+                          html:
+                            '<img src="{{asset('novo')}}/imgs/voucherFinalizado.jpg" style="width:100%;">',
+                          showCloseButton: true,
+                          showCancelButton: false,
+                          focusConfirm: false,
+                          confirmButtonText:
+                            '<i class="fas fa-thumbs-up"></i>',
+                          })
+
+                        setTimeout(function(){ Swal.close() }, 60000);
+
+                    }
+
+                    </script>
+
                 @endif
             </div>
             <h3>PROCEDIMENTOS:</h3>
@@ -24,29 +51,36 @@
             <p>Material biológico: Sangue, fezes e urina.</p>
             <p>(clique no procedimento para saber o que eles podem diagnosticar)</p>
     
-            <h3>Para gerar seu voucher de Checkup pelo WhatsApp</h3>
-            <br>
-            <a onclick='bigCupom()' class="pattern" style="font-size: 20px;">clique aqui</a>
+            
+
+            @if(!$vale)
+                <h3>Para gerar seu voucher de Checkup</h3>
+                <br>
+                <a onclick='bigCupom()' class="pattern" style="font-size: 20px;">clique aqui</a>
+
+                
+                <div class='dNone' id='bigCupom'>
+                  <div class="border">
+                      <a href="#"><i class="fRight fas fa-times-circle" onclick="bigCupom()"></i></a>
+                      <div class="">
+                        <h1>Para fazer a liberação do seu voucher, favor insira seu número de celular abaixo (com DDD) e siga as instruções pós liberação.</h1>
+                        <form action="{{route('checkupValePost')}}" method="POST">
+                            <input type="hidden" name="_token" value="{{ csrf_token() }}" />
+                            <label for="" class="col1">
+                                <input type="number" name="cel" style='width:80%;'>
+                            </label>
+                            <label for="" class="col1">
+                                <button>enviar</button>
+                            </label>
+                        </form>
+                      </div>
+                  </div>
+                </div>
+            @endif
+            
             <br>
             <a href="javascript:history.back()" class='pattern'><span><i class="fas fa-undo-alt"></i> voltar ao menu principal</span></a>
             
-            <div class='dNone' id='bigCupom'>
-              <div class="border">
-                  <a href="#"><i class="fRight fas fa-times-circle" onclick="bigCupom()"></i></a>
-                  <div class="">
-                    <h1>Para receber seu voucher, favor insira seu número de celular abaixo que dentro de 24hrs enviaremos uma mensagem por What's app.</h1>
-                    <form action="{{route('checkupValePost')}}" method="POST">
-                            <input type="hidden" name="_token" value="{{ csrf_token() }}" />
-                        <label for="" class="col1">
-                            <input type="number" name="cel" style='width:80%;'>
-                        </label>
-                        <label for="" class="col1">
-                            <button>enviar</button>
-                        </label>
-                    </form>
-                  </div>
-              </div>
-            </div>
     </section>
 
 @endsection
