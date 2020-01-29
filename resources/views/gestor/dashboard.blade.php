15b5a83d14d939c2d4f6e968983c23a32ba7d0c3
Marcos Bruno <marcosbruno.mb@gmail.com>
2020-01-29 17:42:33 -0300

    Área do Gestor - Melhorias gráficos

--- resources/views/gestor/dashboard.blade.php
+++ resources/views/gestor/dashboard.blade.php
@@ -28,7 +28,7 @@
 
     <div class="row">
 
-      <div class="col-md-6 col-sm-6 col-xs-12">
+      <div class="col-md-12 col-sm-12 col-xs-12">
         <div class="box box-primary no-border">
           <div class="box-body">
               <div class="chart">
@@ -38,7 +38,7 @@
         </div>
       </div>
 
-      <div class="col-md-6 col-sm-6 col-xs-12">
+      <div class="col-md-12 col-sm-12 col-xs-12">
         <div class="box box-primary no-border">
           <div class="box-body">
               <div class="chart">
