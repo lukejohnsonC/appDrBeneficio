f3a7c67c20ae628d80e739aa7d8d0764a5f12d9d
Luke <lukejohnson73@hotmail.com>
2020-03-03 17:34:02 -0300

    Seguro de vida
    tirando img colocando em formato texto

--- resources/views/SeguroDeVida/index.blade.php
+++ resources/views/SeguroDeVida/index.blade.php
@@ -1,9 +1,29 @@
 @extends('estrutura.master') 
 
 @section('conteudo')
-<section id="ori_nutricional">
-        <div class="container">
-            <img src="{{asset('novo')}}/imgs/Group-12-9.svg" alt="" class="image-209" style="width: auto;margin: 0 auto;margin-top: 0px;max-width: 100%;margin-top: 50px;">
-        </div>
+
+<section id="funeral">      
+        
+  <div class='container'>
+      <h1>Seguro de Ac. Pessoais R$ 20 mil</h1>
+      <h3>(em caso de morte acidental)</h3>
+
+      <p>Para acionar o seguro de Ac. Pessoais, ligue para <a href="01332261111" class='pattern'>(13) 3226.1111</a> ou clique no botão laranja (aceita ligações à cobrar)</p>
+
+      <p>Whatsapp <a href="https://wa.me/5513997748080"></a>(13) 9 9774-8080 ou pelo chat<br>
+
+      Atendimento em horário comercial: de segunda a sexta das 9h às 18h<br>
+
+      Site: <a href="www.drbeneficio.com.br" target="_blank">www.drbeneficio.com.br</a></p>
+  
+      <p style='color:#F36F21'>Importante: LEIA COM ATENÇÃO ANTES DE LIGAR!</p>
+
+      <p>Comunique o falecimento e siga as orientações para apresentação dos documentos necessários para a regulação do sinistro.<br>
+      A regulação para pagamento da indenização será concluída em até 30 dias da entrega de todos os documentos solicitados.</p>
+
+    <a href="javascript:history.back()" class='pattern'><span><i class="fas fa-undo-alt"></i> voltar </span></a>
+  </div>
+</section>
+
       </section>
 @endsection
