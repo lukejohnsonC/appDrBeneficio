828d9a8e09051ea6d1e797185da503868b949516
Luke <lukejohnson73@hotmail.com>
2020-03-11 11:39:50 -0300

    Correção texto
    sorteios mensais

--- resources/views/SorteiosMensais/index.blade.php
+++ resources/views/SorteiosMensais/index.blade.php
@@ -4,14 +4,14 @@
 
 <section id="funeral">
 	<div class="container">
-		<h1>Titulo De Captalização</h1>
+		<h1>Titulo De Capitalização</h1>
 
 		<p>Prêmios Mensais. Você participa todo mês de um sorteio no valor total de R$10 mil reais pela loteria federal.</p>
 
 		<h3>Instruções:</h3>
 		
 		<ol id="steps">
-			<li><p><i class="fas fa-pen-fancy"></i> Ao se tornar um Cliente DR. BENEFÍCIO, todo titular ganha um número de Titulo De Captalização</p></li>
+			<li><p><i class="fas fa-pen-fancy"></i> Ao se tornar um Cliente DR. BENEFÍCIO, todo titular e os adicionais ganham um número de Titulo De Capitalização</p></li>
 			<li><p><i class="fas fa-list-ol"></i> Todo mês você fará parte do sorteio pela loteria federal</p></li>
 			<li><p><i class="fab fa-angellist"></i> Todo mês você fará parte do sorteio pela Loteria Federal</p></li>
 			<li><p><i class="fas fa-check-double"></i> Siga as instruções abaixo para conferir se você é O Grande Vencedor. Boa Sorte!</p></li>
