208b82b785fa15f5ba51ba56f95507206766bb3a
Luke <lukejohnson73@hotmail.com>
2020-03-17 15:01:22 -0300

    Logos Atribuna
    para o clube do assinante

--- resources/views/CartaoFarmacia/verCartaoFarmacia.blade.php
+++ resources/views/CartaoFarmacia/verCartaoFarmacia.blade.php
@@ -5,7 +5,7 @@
         <div id='hasBg'>
           <div id="card-verso">
             <span><b>Número de Identificação:</b></span>
-            <span><a class='pattern'><b>{{$nr_rd}}</b></a></span>
+            <span><a class='pattern'>{{$nr_rd}}</a></span>
             <span style='margin-top:3rem;'>Válido em todas as farmácias da rede:</span>
             <img src="{{asset('novo')}}/imgs/partners-2.png">
           </div>
