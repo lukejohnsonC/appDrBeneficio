cec7e77716b29781290640ef1b9d35da22ade988
Luke <lukejohnson73@hotmail.com>
2020-03-18 14:13:28 -0300

    Imagens + Cartão
    Imagens pro Clube de Vantagens + cartão clube do assinante

--- resources/views/CartaoTribuna/index.blade.php
+++ resources/views/CartaoTribuna/index.blade.php
@@ -7,40 +7,49 @@
 	<div class="container">
 		<style>
 			.cartao {
-				width: 465px;
+			    max-width: 465px;
+			    width: 100%;
 				height: 275px;
 				margin: 0 auto;
-				background: #0e9ff8;
-				border-radius: 15px;
+				background-image: url("{{asset('novo/imgs')}}/cartaoAtribuna.jpg");
+				background-size: contain;
+				background-repeat: no-repeat;
+				position: relative;
 			}
-				.cartao .logoAtribuna {
-				    width: 20%;
-				    margin-left: 5%;
-				    position: relative;
-				    top: 5rem;
+			.cartao.verso {
+				background-image: url("{{asset('novo/imgs')}}/cartaoAtribunaVerso.jpg");
+			}
+				.cartao .imagensLogo row {
+				    margin-top: 37%;
+				    margin-left: 10%;
+				    width: 80%;
 				    display: inline-block;
 				}
-					.cartao .logoAtribuna img {
-						width: 100%;
+					.cartao .imagensLogo img {
+					    max-width: 170px;
+					    float: left;
+					    width: 46%;
+					}
+					.cartao .imagensLogo img.fRight {
+						float: right;
 					}
 				.cartao .texto {
-				    width: 70%;
-				    float: right;
+				    width: 80%;
+				    height: 155px;
 				    text-transform: uppercase;
-				    margin-top: 4rem;
-				    line-height: 40px;
-				    font-size: 16px;
+				    line-height: 30px;
+				    font-size: 14px;
 				    color: #FFF;
+				    position: absolute;
+				    top: 2.5rem;
+				    left: 35px;
 				}
 					.cartao .texto row {
-						display: block;
+						display: inline-block;
 					}
 						.cartao .texto row .titulo {
-							text-decoration: underline;
-						    width: 28%;
 						    display: inline-block;
-						    text-align: right;
-						    height: 30px;
+						    margin-right: 5px;
 						}
 						.cartao .texto row .subtit {
 							padding: 5px;
@@ -48,35 +57,57 @@
 							border-left: 1px solid #FFF;
 						}
 
-			.cartao2 {
-			    width: 100%;
-			    max-width: 465px;
-			    margin: 2rem auto;
-			    display: block;
-			}
-				.cartao2 img {
-					width: 100%;
+			@media only screen and (max-width: 425px) {
+				.cartao {
+					height: 185px;
 				}
+					.cartao .texto {
+					    width: 80%;
+					    height: 100px;
+					    text-transform: uppercase;
+					    line-height: 20px;
+					    font-size: 10px;
+					    color: #FFF;
+					    position: absolute;
+					    top: 1rem;
+					    left: 22px;
+					}
+			}
 		</style>
 
-    <div class="cartao">
-			<div class="logoAtribuna">
-				<img src="{{asset('novo/imgs')}}/logo_atribuna.png" alt="Logo Clube do Assinante">
+		<div class="cartao">
+			<div class="imagensLogo">
+				<row>
+					<img src="{{asset('novo/imgs')}}/logoDrBranco.png" alt="" class='fRight'>
+				</row>
 			</div>
+		</div>
 
+	    <div class="cartao verso">
 			<div class="texto">
 				<row><span class="titulo">NOME</span><span class='subtit'>{{$data->nmCliente}}</span></row>
 				<row><span class="titulo">CÓDIGO DO ASSINANTE</span><span class='subtit'>{{$data->nuCliente}}</span></row>
-				<row><span class="titulo">CÓDIGO RAIA/DROGASIL (UNIVERS)</span><span class='subtit'>{{$nr_rd}}</span></row>
 				<row><span class="titulo">REDE DE SAÚDE DR. BENEFÍCIO</span><span class='subtit'>SIM</span></row>
 				<row><span class="titulo">Validade</span><span class='subtit'>{{$validade}}</span></row>
+				<row style='
+				    color: #2AA4D5;
+				    position: absolute;
+				    bottom: -27px;
+				    width: 100%;
+				    left: 0px;'>
+				    <span class="titulo">CÓDIGO RAIA/DROGASIL</span><span class='subtit'>{{$nr_rd}}</span>
+				</row>
+				<row style='width: 100%;
+				    position: absolute;
+				    left: 0;
+				    bottom: -40px;'>
+				    <img src="{{asset('novo/imgs')}}/parceiros.png" style='
+				    display: block;
+				    margin: 0 auto;
+				    width: 60%;'>
+				</row>
 			</div>
-
 		</div>
-
-{{--
-		<div class="cartao2"><img src="{{asset('novo/imgs')}}/clube_cartao.png" alt="Cartão do Clube do Assinante"></div>
---}}
 
 	</div>
 </section>
