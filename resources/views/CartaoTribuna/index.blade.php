c0d2f6614c3a0028f777225af41fbc79b0d47683
Marcos Bruno <marcosbruno.mb@gmail.com>
2020-03-24 15:16:56 -0300

    Alteracao de lógica cartao tribuna

--- resources/views/CartaoTribuna/index.blade.php
+++ resources/views/CartaoTribuna/index.blade.php
@@ -2,31 +2,6 @@
 
 @section('conteudo')
 
-
-<section id="funeral" class=''>
-	<div class="container">
-		<style>
-			.cartao {
-			    max-width: 465px;
-			    width: 100%;
-				height: 275px;
-				margin: 0 auto;
-				background-image: url("{{asset('novo/imgs')}}/cartaoAtribuna.jpg");
-				background-size: contain;
-				background-repeat: no-repeat;
-				position: relative;
-			}
-
-			@media only screen and (max-width: 425px) {
-				.cartao {
-					height
-				}
-			}
-		</style>
-
-		<div class="cartao">
-		</div>
-	</div>
-</section>
+@include('CartaoTribuna.cartaotribuna')
 
 @endsection
