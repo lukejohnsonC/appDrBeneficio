0f58697662983d2318530a5ed3ab6b44106bc26b
Luke <lukejohnson73@hotmail.com>
2020-03-25 11:23:45 -0300

    correção

--- resources/views/css/main.blade.php
+++ resources/views/css/main.blade.php
@@ -170,7 +170,7 @@ form {
 		    width: 85px;
 		    height: auto;
 		    text-align: center;
-		    background: {{primary}};
+		    background: {{$primary}};
 		    margin-top: 45px;
 		    color: #FFF;
 		    padding: 5px;
