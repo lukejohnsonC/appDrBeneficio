3166fef5a66acdee4aa5f09ba2affd5345ec2131
Luke <lukejohnson73@hotmail.com>
2020-03-30 12:44:24 -0300

    Alterações e melhorias Cartao Atribuna

--- resources/views/CartaoTribuna/cartaotribuna.blade.php
+++ resources/views/CartaoTribuna/cartaotribuna.blade.php
@@ -4,7 +4,7 @@
 			.cartao {
 			    max-width: 550px;
 			    width: 100%;
-				height: 330px;
+				height: 356px;
 				margin: 0 auto;
 				background-image: url("{{asset('novo/imgs')}}/cartaoAtribuna.jpg");
 				background-size: contain;
@@ -14,17 +14,21 @@
 				#info {
 				    position: absolute;
 				    width: 80%;
-				    top: 25%;
+				    top: 24%;
 				    left: 5%;
 				    color: white;
+				    text-transform: capitalize;
+				    font-size: 14px;
 				}
 					#info span {
 						display: block;
+    					margin-bottom: 0.5rem;
 					}
 
 			@media (min-width: 460px) {
 				#info {
 					top: 35%;
+					font-size: 100% !important;
 				}
 			}
 			@media (min-width: 600px) {
@@ -38,6 +42,7 @@
 			<div id="info">
 				<span>Nome: {{$data->nmCliente}}</span>
 				<span>Código do Assinante: {{$data->nuCliente}}</span>
+				<span>Rede de saúde Dr. Benefício: SIM</span>
 				<span>Validade: {{$validade}}</span>
 			</div>
 		</div>
