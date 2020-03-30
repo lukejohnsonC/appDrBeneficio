4f06e2f0209acf7c958e8a63943763d7c46efe4e
Luke <lukejohnson73@hotmail.com>
2020-03-30 11:04:27 -0300

    Alterações cartão atribuna
    cartão + logos

--- resources/views/CartaoTribuna/cartaotribuna.blade.php
+++ resources/views/CartaoTribuna/cartaotribuna.blade.php
@@ -11,9 +11,31 @@
 				background-repeat: no-repeat;
 				position: relative;
 			}
+				#info {
+				    position: absolute;
+				    width: 80%;
+				    top: 25%;
+				    left: 5%;
+				    color: white;
+				}
+
+			@media (min-width: 460px) {
+				#info {
+					top: 35%;
+				}
+			}
+			@media (min-width: 600px) {
+				#info {
+					top: 50%;
+				}
+			}
 		</style>
 
 		<div class="cartao">
+			<div id="info">
+				<span>Nome: Nome do Fulano por Completo Aqui</span>
+				<span>Código do Assinante: 1234566789</span>
+			</div>
 		</div>
 	</div>
 </section>
