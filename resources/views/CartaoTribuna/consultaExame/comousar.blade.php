d833f9b93c16a010556215f3a9476a0707cd1dff
Marcos Bruno <marcosbruno.mb@gmail.com>
2020-03-31 19:15:48 -0300

    Textos como usar

--- resources/views/CartaoTribuna/consultaExame/comousar.blade.php
+++ resources/views/CartaoTribuna/consultaExame/comousar.blade.php
@@ -4,8 +4,15 @@
 
 
 <section class='container' style="min-height:auto;">
-		<p>como usar consulta exame
-		preencher aqui como usar</p>
+		<p>Caro ASSINANTE, o cartão abaixo lhe dá acesso a toda rede de saúde Dr. Benefício.
+<br /><br />
+As melhores clínicas médicas, odontológicas e laboratórios de nossa região com condições diferenciadas.
+<br /><br />
+Para agendar uma consulta ou exame, entre em contato diretamente com cada clínica pelo telefone informado e informe que você é um cliente DR. BENEFÍCIO para ter acesso a nossa tabela de preços diferenciada.
+<br /><br />
+Em caso de dúvidas, entre em contato com a Central de Atendimento Dr. Benefício através do telefone 13-32261111 ou por whatsapp (clique no botão abaixo).
+<br /><br />
+IMPORTANTE: Benefício válido somente para o ASSINANTE titular (não extensivo para familiares e dependentes).</p>
 </section>
 
 
--- resources/views/CartaoTribuna/raiaDrogasil/comousar.blade.php
+++ resources/views/CartaoTribuna/raiaDrogasil/comousar.blade.php
@@ -2,19 +2,21 @@
 
 @section('conteudo')
 
-<section class='container' style="min-height:auto;display:none;">
-<p>como usar raia drogasil
-preencher aqui como usar
 
 
-NUMERO DO RD: {{$nr_rd}}</p>
-</section>
 
 @isset($nr_rd_nao_existe)
   <div class="container">
     <h3>Prezado Usuário, o seu Cartão de Descontos para as Redes Drogasil e Raia, está sendo processado em nossos sistemas. Em breve o seu cartão estará disponível. Obrigado!</h3>
   </div>
 @else
+<section class='container' style="min-height:auto;">
+  <p>Caro ASSINANTE, apresente o cartão abaixo em qualquer farmácia da rede DROGA RAIA ou DROGASIL para obter desconto em seu medicamento (se solicitado, informe também seu CPF).
+<br /><br />
+  NÃO ESQUEÇA DE MENCIONAR AO BALCONISTA/FARMACÊUTICO que você é um cliente DR. BENEFÍCIO, que faz parte da rede UNIVERSE. Caso o problema persista, peça para ele contatar a central de atendimento UNIVERSE.
+<br /><br />
+  IMPORTANTE: Benefício válido somente para o ASSINANTE titular (não extensivo para familiares e dependentes).</p>
+</section>
 <section id="cartao_virtual" class=''>
   @include('CartaoFarmacia.cartaofarmacia')
 </section>
