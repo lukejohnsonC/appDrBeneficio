95eebc10b513febbd3134a69adb90646f98e2ac7
Marcos Bruno <marcosbruno.mb@gmail.com>
2020-04-01 09:54:07 -0300

    Como usar aopharmaceutico

--- resources/views/CartaoTribuna/aoPharmaceutico/comousar.blade.php
+++ resources/views/CartaoTribuna/aoPharmaceutico/comousar.blade.php
@@ -3,8 +3,9 @@
 @section('conteudo')
 
 <section class='container' style="min-height:auto;">
-<p>como usar ao pharmaceutico
-preencher aqui como usar</p>
+<p>Para usufruir às vantagens e descontos exclusivos, basta apresentar seu cartão nos estabelecimentos conveniados, juntamente com um documento com foto.
+<br /><br />
+O cartão do Clube é pessoal e intransferível.</p>
 </section>
 
 
